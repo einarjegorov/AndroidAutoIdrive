560c1f70390f956f4953d53081ef0b3f33547dad
Walter Huf <hufman@gmail.com>
2020-10-22 13:18:32 -0700

    Set cuttype WORDS_AUTO_STATIC_TEXT on the enqueued title components

--- app/src/main/java/me/hufman/androidautoidrive/carapp/music/views/EnqueuedView.kt
+++ app/src/main/java/me/hufman/androidautoidrive/carapp/music/views/EnqueuedView.kt
@@ -71,6 +71,9 @@ class EnqueuedView(val state: RHMIState, val musicController: MusicController, v
 	}
 
 	fun initWidgets(playbackView: PlaybackView) {
+		titleLabelComponent.setProperty(RHMIProperty.PropertyId.CUTTYPE, 4)
+		subtitleLabelComponent.setProperty(RHMIProperty.PropertyId.CUTTYPE, 4)
+
 		// this is required for pagination system
 		listComponent.setProperty(RHMIProperty.PropertyId.VALID, false)
 
